d7d5c3cc172c37cf440a4d9176e31c943e2645d2
Andrzej Jędrzejczak <jedrzejczak.andrzej89@gmail.com>
2019-01-05 20:00:00 +0100

    problem 003

--- src/solutions.js
+++ src/solutions.js
@@ -1,5 +1,7 @@
 import solution001 from "./001";
 import solution002 from "./002";
+import solution003 from "./003";
 
 console.log(`Solution for 001 is ${solution001(10)}`);
 console.log(`Solution for 002 is ${solution002(4000000)}`);
+console.log(`Solution for 003 is ${solution003(600851475143)}`);
